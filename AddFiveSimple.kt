957cd9d20ae5795bf6bd270c42b6d4f24b52a854
Neloy007 <neloymac24@gmail.com>
2025-06-01 16:53:54 +0600

    add five numbers in a simple way

---
+++ AddFiveSimple.kt
@@ -0,0 +1,16 @@
+fun main() {
+
+    print("Enter number 1: ")
+    val a =readLine()!!.toInt()
+    print("Enter number 2: ")
+    val b =readLine()!!.toInt()
+    print("Enter number 3: ")
+    val c =readLine()!!.toInt()
+    print("Enter number 4: ")
+    val d =readLine()!!.toInt()
+    print("Enter number 5: ")
+    val e =readLine()!!.toInt()
+    
+    val sum = a+b+c+d+e
+    print("The sum of 5 numbers is: $sum")
+}
