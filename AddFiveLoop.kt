6eba5f214f69c157ab13d39b03436fb412e5309b
Neloy007 <neloymac24@gmail.com>
2025-06-01 19:37:13 +0600

    if-else is replaced by when

--- AddFiveLoop.kt
+++ AddFiveLoop.kt
@@ -2,24 +2,18 @@ fun main() {
     var sum = 0
 
     var i = 0
-    while (i < 10) {
+    while (i < 5) {
         println("Enter number ${i+1}: ")
         
         val number = readLine()?.toIntOrNull()
 
-        if (number != null) {
-            sum += number
-            i++
-        }else{
-            println("Please enter a valid Integer.")
-        }
-
-
-        if (i == 5) {
-            break
-        }
+         when {
+           number == null -> println("Please enter a valid number.")
+           else -> {
+               sum += number
+               i++
+           }
     }
-
 
     println("The sum of the 5 numbers is: $sum")
 }
